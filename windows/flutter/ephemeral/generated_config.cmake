90a729a6a54b1db9bb398d815fc39bcc149d997b
karnati-kalyani-reddy <121938123+karnati-kalyani-reddy@users.noreply.github.com>
2024-08-09 15:20:31 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\91934\\Downloads\\flutter_windows_3.24.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\91934\\Documents\\reminder_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\91934\\Downloads\\flutter_windows_3.24.0-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\91934\\Documents\\reminder_app"
+  "FLUTTER_ROOT=C:\\Users\\91934\\Downloads\\flutter_windows_3.24.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\91934\\Documents\\reminder_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\91934\\Documents\\reminder_app"
+  "FLUTTER_TARGET=C:\\Users\\91934\\Documents\\reminder_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\91934\\Documents\\reminder_app\\.dart_tool\\package_config.json"
+)
